508d48414b83d942cc40df530e40397955ca663d
pajowu <git@ca.pajowu.de>
2022-01-04 03:41:48 +0100

    ✨ Pass test_id as url param

--- src/routes/Result.tsx
+++ src/routes/Result.tsx
@@ -6,17 +6,18 @@ import data from '../data/all.json';
 import {get_test} from "../utils/testdata";
 import NoTestFound from "../components/Result/NoTestFound";
 import TestFound from "../components/Result/TestFound";
+import {useSearchParams} from "react-router-dom";
 
 export default function Result() {
-
+    const [searchParams, setSearchParams] = useSearchParams();
     let params = useParams();
     let navigate = useNavigate();
-    if (params.testId === undefined) {
+    let test_id = searchParams.get('test_id')
+    if (test_id === null) {
         navigate("/scan")
         return <></>
     }
 
-    let test_id = params.testId;
     let test_data = get_test(test_id);
-    return test_data == null ? <NoTestFound/> : <TestFound testdata={test_data}/>
+    return test_data == null ? <NoTestFound test_id={test_id}/> : <TestFound testdata={test_data}/>
 }
