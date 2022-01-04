6c28fa1b360adbe87d35f71e3dd4fb7193f5e478
pajowu <git@ca.pajowu.de>
2022-01-04 02:04:55 +0100

    ✨ Add barcode scanner and result handler

--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,21 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import {
+    BrowserRouter,
+    Routes,
+    Route
+} from "react-router-dom";
+import Scanner from "./routes/Scanner";
+import Result from "./routes/Result";
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<App />} />
+            <Route path="scan" element={<Scanner />} />
+            <Route path="result/:testId" element={<Result />} />
+        </Routes>
+    </BrowserRouter>,
   document.getElementById('root')
 );
 
