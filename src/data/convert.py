15993bff55c3805dac373df229110cb254d04d35
pajowu <git@ca.pajowu.de>
2022-01-04 11:06:13 +0100

    🍱 cleanup at-nrs

--- src/data/convert.py
+++ src/data/convert.py
@@ -17,11 +17,18 @@ def cleaned_value(value):
 def clean_row(source):
     translation = {"AT-Nr. / AT-No. ": "at_nr","Ref-Nr./ ID-No. * ":"ref_nr","Hersteller / Manufacturer ": "manufacturer","Testname / Test name ": "test_name","Zielantigen / target antigen ":"target_antigen","Cq <25 ":"sensitivity_cq<25","Cq 25-30 ": "sensitivity_cq25-30","Cq >30 ":"sensitivity_cq>30","Gesamt- Sensitvität / total sensitivity ": "sensitivity_total"}
     return {translation.get(k, k.strip()): cleaned_value(v) for k,v in source.items() if k}
+def split_at_nrs(at_nr):
+    match = re.match(r'(AT\d{3}/\d{2}) \((AT\d{3}/\d{2})\)', at_nr)
+    if match:
+        return match.groups() + (at_nr,)
+    else:
+        return [at_nr]
 
 reader = csv.DictReader(args.input)
 data = {}
 for row in reader:
     cleaned_row = clean_row(row)
-    data[cleaned_row['at_nr']] = cleaned_row
+    for at_nr in split_at_nrs(cleaned_row['at_nr']):
+        data[at_nr] = cleaned_row
 
 json.dump(data, args.output, indent='  ')
