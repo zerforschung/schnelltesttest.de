0e0258dc70287231ae484780d734069ee707d538
pajowu <git@ca.pajowu.de>
2022-01-13 03:37:53 +0100

    ✨ load fonts locally

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,6 @@ import './App.css';
 import Logo from './components/Logo';
 import { BigLinkButton } from './components/Buttons';
 
-import WebFont from 'webfontloader';
-
-WebFont.load({
-  google: {
-    families: [
-      'Open Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700',
-      'Open Sans Condensed:ital,wght@0,300;0,700;1,300',
-      'sans-serif',
-    ],
-  },
-});
-
 function App(): JSX.Element {
   return (
     <div
