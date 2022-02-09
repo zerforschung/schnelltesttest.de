e18c65773015be3adc016a06dcbebb9dd3b26d44
pajowu <git@ca.pajowu.de>
2022-02-09 03:20:49 +0100

    🍱 ean map

--- src/data/sheet2map.py
+++ src/data/sheet2map.py
@@ -2,17 +2,21 @@ import csv
 import json
 import sys
 
+SKIP_LIST = ["-", "#REF!"]
 data = {}
 with open(sys.argv[1], encoding="utf-8-sig") as f:
     reader = csv.DictReader(f)
     for row in reader:
         at_nr = row["AT-Nr. / AT-No."]
         ids = list(
-            set(x.strip() for x in row["EANs alle"].split(";") if x.strip().isdigit())
+            set(x.strip() for x in row["EANs alle"].split(";") if x.strip() and x.strip() not in SKIP_LIST)
         )
+        for i in ids:
+            if not i.isdigit():
+                print("Warning, non-digit ean", i)
         for id in ids:
             if id in data:
-                print("Duplicate EAN:", id)
+                print("ERROR: Duplicate EAN:", id)
                 continue
             data[id] = at_nr
 
